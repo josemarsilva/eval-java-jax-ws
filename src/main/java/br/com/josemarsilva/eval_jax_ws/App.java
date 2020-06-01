a1c7a5d10e2abe57a3426a4709aa3578f7bba4bd
josemarsilva <josemar.furegatti@gmail.com>
2020-06-01 20:27:43 -0300

    doc

--- src/main/java/br/com/josemarsilva/eval_jax_ws/App.java
+++ src/main/java/br/com/josemarsilva/eval_jax_ws/App.java
@@ -1,12 +1,11 @@
-package br.com.correios.josemarsilva.eval_jax_ws;
+package br.com.josemarsilva.eval_jax_ws;
 
 import br.com.correios.bsb.sigep.master.bean.cliente.ConsultaCEP;
 
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("br.com.correios.josemarsilva.eval_jax_ws.main()");
-		
+		System.out.println("main()");
 		ConsultaCEP consultaCEP = new ConsultaCEP();
 		consultaCEP.setCep("13050410");
 		System.out.println( consultaCEP.getCep() );
